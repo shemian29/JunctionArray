b3c31a1f9a3099fc69e39c07e3327aed4e3baf9c
shemian29 <ims.vault.alpha@gmail.com>
2023-02-03 18:05:02 -0600

    Start JJCircuit class

---
+++ JJArray/JJCircuit.py
@@ -0,0 +1,17 @@
+
+
+import JJArray as jja
+import numpy as np
+import qutip as qt
+import matplotlib.pyplot as plt
+from scipy.sparse import bmat
+from tqdm.notebook import tqdm
+from scipy.sparse import csr_matrix
+import scqubits as scq
+
+
+class
+
+class JJCircuit:
+
+    def __init__(self):
