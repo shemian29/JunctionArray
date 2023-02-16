79df9a0dc77db485165f9a8ceac6028f85254530
shemian29 <ims.vault.alpha@gmail.com>
2023-02-16 15:24:50 -0600

    Refactor junction array class

--- JJArray/__init__.py
+++ JJArray/__init__.py
@@ -2,4 +2,4 @@
 # from JJArray.symmetry import amps, ChargeToTranslation, GenerateMomentumBasis, SectorDiagonalization, SectorDiagonalization_Energies, SortedDiagonalization, SortedDiagonalization_Energies
 # from JJArray.wigner import cartesian, WignerArray, WignerPoint, Wmatrix
 # from JJArray.basis import ind2occ,ind2state,Ket,state2ind, T1, ToKet, TransInd
-from JJArray.JJCircuit import JJCircuit
+from JJArray.junction_array import junction_array
